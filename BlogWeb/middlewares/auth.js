ae016befec0ecdf0d1e3305e2439687111bbf698
Durgesh-05 <durgeshkumar.d@somaiya.edu>
2024-05-29 22:57:22 +0530

    update auth.js

--- BlogWeb/middlewares/auth.js
+++ BlogWeb/middlewares/auth.js
@@ -2,7 +2,7 @@ import jwt from "jsonwebtoken";
 function validateAccessToken() {
   return (req, res, next) => {
     const accessTokenValue = req.cookies["accessToken"];
-    if (!accessTokenValue) return res.render("homepage");
+    if (!accessTokenValue) return next();
     try {
       const userPayload = jwt.verify(accessTokenValue, process.env.SECRET_KEY);
       req.user = userPayload;
